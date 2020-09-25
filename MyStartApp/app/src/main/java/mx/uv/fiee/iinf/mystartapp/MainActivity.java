5b7e81e799872ea3fa16a730ae3b349ccd685d89
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2020-09-25 00:30:30 -0500

    Add image loaded from resources

--- MyStartApp/app/src/main/java/mx/uv/fiee/iinf/mystartapp/MainActivity.java
+++ MyStartApp/app/src/main/java/mx/uv/fiee/iinf/mystartapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,19 +13,19 @@ import androidx.annotation.Nullable;
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate (@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
 
         final TextView tvVal1 = findViewById (R.id.tvVal1);
         TextView tvVal2 = findViewById (R.id.tvVal2);
         Button btnPress = findViewById (R.id.next_button);
+        ImageView iv = findViewById (R.id.image1);
 
-        btnPress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText (getBaseContext(), tvVal1.getText (), Toast.LENGTH_LONG).show ();
-            }
+        btnPress.setOnClickListener(view -> {
+            iv.setImageResource (R.mipmap.ic_launcher_round);
         });
+
+
     }
 }
