be79f1e154aa57f3941a994d233ffaaa6daef3d6
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2020-10-08 12:57:16 -0500

    Lists with ListView & RecylerView

--- MP3Player/app/src/main/java/mx/uv/fiee/iinf/mp3player/MainActivity.java
+++ MP3Player/app/src/main/java/mx/uv/fiee/iinf/mp3player/MainActivity.java
@@ -7,10 +7,17 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends Activity {
     public static final int REQUEST_CODE = 1001;
@@ -21,29 +28,39 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnItem).setOnClickListener (v -> {
-            Intent intent = new Intent (getBaseContext (), DetailsActivity.class);
-            intent.putExtra ("AUDIO", "/sdcard/Music/01.mp3");
-            startActivityForResult (intent, ACTIVITY_REQUEST_CODE);
+        // origen de datos
+        String [] days = { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes" };
 
+        // controlador
+        ArrayAdapter<String> adapter = new ArrayAdapter<> (getBaseContext(), android.R.layout.simple_list_item_2, days);
 
-            // verifica si se cuenta con el permiso para realizar llamadas
-//            int perm = getBaseContext ().checkSelfPermission (Manifest.permission.CALL_PHONE);
-//            if (perm != PackageManager.PERMISSION_GRANTED) { // sino se tiene el permiso
-//                requestPermissions ( // se solicita explicitamente
-//                        new String [] { Manifest.permission.CALL_PHONE, Manifest.permission.READ_EXTERNAL_STORAGE },
-//                        REQUEST_CODE
-//                );
+        // vista
+        RecyclerView lv = findViewById (R.id.list1);
+        lv.setAdapter (new MyAdapter ());
+
+//        findViewById(R.id.btnItem).setOnClickListener (v -> {
+//            Intent intent = new Intent (getBaseContext (), DetailsActivity.class);
+//            intent.putExtra ("AUDIO", "/sdcard/Music/01.mp3");
+//            startActivityForResult (intent, ACTIVITY_REQUEST_CODE);
 //
-//                return;
-//            }
 //
-//            Invoca al marcador telefónico
-//            String uri = "tel: 229 2343435";
-//            Intent intent = new Intent (Intent.ACTION_CALL);
-//            intent.setData (Uri.parse (uri));
-//            startActivity (intent);
-        });
+//            // verifica si se cuenta con el permiso para realizar llamadas
+////            int perm = getBaseContext ().checkSelfPermission (Manifest.permission.CALL_PHONE);
+////            if (perm != PackageManager.PERMISSION_GRANTED) { // sino se tiene el permiso
+////                requestPermissions ( // se solicita explicitamente
+////                        new String [] { Manifest.permission.CALL_PHONE, Manifest.permission.READ_EXTERNAL_STORAGE },
+////                        REQUEST_CODE
+////                );
+////
+////                return;
+////            }
+////
+////            Invoca al marcador telefónico
+////            String uri = "tel: 229 2343435";
+////            Intent intent = new Intent (Intent.ACTION_CALL);
+////            intent.setData (Uri.parse (uri));
+////            startActivity (intent);
+//        });
     }
 
     /**
@@ -78,4 +95,32 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder (@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount () {
+        return 0;
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder (@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
